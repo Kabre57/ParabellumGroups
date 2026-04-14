ecc50a275089d1607000d4e35cc636ac86f92baa
Kabre57 <theogeoffroy5@gmail.com>
2026-04-14 20:51:00 +0000

    fix: proxy storage Minio - path rewrite au bon niveau

--- services/api-gateway/routes/services/storage.routes.js
+++ services/api-gateway/routes/services/storage.routes.js
@@ -1,17 +1,14 @@
-const config = require('../../utils/config');
-
 module.exports = {
   serviceName: 'storage-service',
   basePath: 'http://minio:9000',
-  pathRewrite: {
-    '^/api/storage': ''
-  },
   routes: [
     {
       path: '/storage/*',
       method: 'use',
-      // No auth requirement for public image viewing
-      auth: false
+      auth: false,
+      pathRewrite: {
+        '^/api/storage': ''
+      }
     }
   ]
 };
